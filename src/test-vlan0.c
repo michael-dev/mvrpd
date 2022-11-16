6220b959edeff93d45fab09810762b089dcc87d4
michael-dev <michael-dev@fami-braun.de>
2022-11-16 13:28:37 +0100

    Update test-vlan0.c
    Use smaller functions

--- src/test-vlan0.c
+++ src/test-vlan0.c
@@ -10,16 +10,8 @@ void dump_vlan(struct vlan_arr *arr) {
 	printf("%s%s\n", tmp, trunc ? "..." : "");
 }
 
-int main(int argc, char *argv[])
-{
-	struct vlan_arr *arr, *arr0;
-
-	parse_cmdline(argc, argv);
-
-	char *argv2[2];
-	argv2[0] = argv[0];
-	argv2[1] = "--debug-all";
-	parse_cmdline(2, argv2);
+void test0() {
+	struct vlan_arr *arr;
 
 	arr = vlan_alloc("test0");
 	vlan_set(arr, 32);
@@ -27,6 +19,10 @@ int main(int argc, char *argv[])
 	vlan_set(arr, 16);
 	dump_vlan(arr);
 	vlan_free(arr);
+}
+
+void test1() {
+	struct vlan_arr *arr;
 
 	arr = vlan_alloc("test1");
 	vlan_set(arr, 31);
@@ -34,13 +30,21 @@ int main(int argc, char *argv[])
 	vlan_set(arr, 15);
 	dump_vlan(arr);
 	vlan_free(arr);
+}
 
+void test2() {
+	struct vlan_arr *arr;
+	
 	arr = vlan_alloc("test2");
 	vlan_set(arr, 1);
 	vlan_set(arr, 31);
 	vlan_set(arr, 15);
 	dump_vlan(arr);
-	vlan_free(arr);
+	vlan_free(arr);	
+}
+
+void test3() {
+	struct vlan_arr *arr;
 
 	arr = vlan_alloc("test3");
 	vlan_set(arr, 1024);
@@ -48,13 +52,21 @@ int main(int argc, char *argv[])
 	vlan_set(arr, 1040);
 	dump_vlan(arr);
 	vlan_free(arr);
+}
 
+void test4() {
+	struct vlan_arr *arr;
+	
 	arr = vlan_alloc("test4");
 	vlan_set(arr, 1024);
 	vlan_set(arr, 1088);
 	vlan_set(arr, 1056);
 	dump_vlan(arr);
 	vlan_free(arr);
+}
+
+void test5() {
+	struct vlan_arr *arr;
 
 	arr = vlan_alloc("test5");
 	vlan_set(arr, 2048);
@@ -63,6 +75,10 @@ int main(int argc, char *argv[])
 	vlan_set(arr, 1056);
 	dump_vlan(arr);
 	vlan_free(arr);
+}
+
+void test6() {
+	struct vlan_arr *arr;
 
 	arr = vlan_alloc("test6");
 	vlan_set(arr, 2048);
@@ -70,14 +86,21 @@ int main(int argc, char *argv[])
 	vlan_set(arr, 1);
 	dump_vlan(arr);
 	vlan_free(arr);
+}
 
+void test7() {
+	struct vlan_arr *arr;
+	
 	arr = vlan_alloc("test7");
 	for (int i = 0; i < 64; i++)
 		vlan_set(arr, i*32+1);
 	vlan_test(arr, 30*32);
 	dump_vlan(arr);
 	vlan_free(arr);
+}
 
+void test8() {
+	struct vlan_arr *arr, *arr0;
 	arr = vlan_alloc("test8a");
 	for (int i = 0; i < 64; i++)
 		vlan_set(arr, i*32+1);
@@ -112,6 +135,10 @@ int main(int argc, char *argv[])
 	dump_vlan(arr0);
 	vlan_free(arr);
 	vlan_free(arr0);
+}
+
+void test9() {
+	struct vlan_arr *arr, *arr0;
 
 	int step = 48;
 	int cnt = 16;
@@ -151,6 +178,26 @@ int main(int argc, char *argv[])
 	dump_vlan(arr0);
 	vlan_free(arr);
 	vlan_free(arr0);
+}
 
+int main(int argc, char *argv[])
+{
+	parse_cmdline(argc, argv);
+
+	char *argv2[2];
+	argv2[0] = argv[0];
+	argv2[1] = "--debug-all";
+	parse_cmdline(2, argv2);
+
+	test0();
+	test1();
+	test2();
+	test3();
+	test4();
+	test5();
+	test6();
+	test7();
+	test8();
+	test9();
 }
 
